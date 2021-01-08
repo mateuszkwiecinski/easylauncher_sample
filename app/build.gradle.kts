1fe45d490c35066f786114cc760d2ba6b8061ff9
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-01-09 00:23:03 +0100

    kts sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,33 @@
 plugins {
-    id("com.starter.application.android") version "0.18.0"
-    id("com.starter.easylauncher") version "3.2.1"
+    id("com.starter.application.android")
+    id("com.starter.easylauncher")
 }
-
 android {
     defaultConfig {
         applicationId = "com.example"
         minSdkVersion(21)
     }
+    buildTypes {
+        named("debug")
+        named("release")
+        register("beta")
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+}
+
+easylauncher {
+    iconNames.add("@mipmap/ic_launcher") // optional, disables automatic launcher icon discovery and will use provided icons only
+
+    buildTypes {
+        register("beta") {
+            // icon names can also be provided per each configuration (buildType, productFlavor or variant)
+            iconNames.add("@mipmap/ic_launcher")
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-subprojects {
-    buildscript {
-        repositories {
-            google()
-        }
-    }
+buildscript {
+    repositories.google()
+}
+plugins {
+    id("com.starter.application.android") version "0.21.1" apply false
+    id("com.starter.easylauncher") version "3.9.0" apply false
 }
