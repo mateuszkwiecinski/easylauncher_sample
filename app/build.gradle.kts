43629ddbf202b73fb532c97bd658716aabc77002
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-10-20 13:24:51 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.example"
-        minSdkVersion(21)
+        minSdk = 21
     }
     buildTypes {
         named("debug")
@@ -15,10 +15,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
 }
 
 easylauncher {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ buildscript {
     repositories.google()
 }
 plugins {
-    id("com.starter.application.android") version "0.21.1" apply false
-    id("com.starter.easylauncher") version "3.9.0" apply false
+    id("com.starter.application.android") version "0.38.0" apply false
+    id("com.starter.easylauncher") version "4.1.1" apply false
 }
